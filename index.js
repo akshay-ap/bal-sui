6fbbd7aec1af197b1248de6a3c8c354f2e3588bc
Akshay <akshay.ap95@gmail.com>
2022-12-29 22:52:04 +0100

    Add query and invoke apis

--- index.js
+++ index.js
@@ -6,35 +6,177 @@ import {
 } from "@mysten/sui.js";
 import { fromB64 } from "@mysten/bcs";
 import * as dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
 
 dotenv.config();
 // connect to local RPC server
 const provider = new JsonRpcProvider(Network.LOCAL);
+
 const privateKey = process.env.PRIVATE_KEY;
 const secretKey = fromB64(privateKey);
 const keypair = Ed25519Keypair.fromSecretKey(secretKey);
 
-const getPublicKey = async () => {
-  return "0x" + keypair.getPublicKey().toSuiAddress();
-};
+const app = express();
 
-console.log(await getPublicKey());
+const subscriptions = [];
+let lastTransactionScannedCount = await provider.getTotalTransactionNumber();
 
-const callMove = async () => {
+app.use(bodyParser.json());
+
+app.post("/execute", async (req, res, next) => {
+  try {
+    const data = req.body;
+    console.log("Execute called with body:", data);
+
+    await callMove(
+      data.packageObjectId,
+      data.module,
+      data.function,
+      data.typeArguments,
+      data.arguments
+    );
+    res.json({ result: "successful" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/query", async (req, res, next) => {
+  try {
+    const data = req.body;
+    const functionIdentifier = data["functionIdentifier"];
+    const eventIdentifier = data["eventIdentifier"];
+
+    const filter = data["filter"];
+    const timeframe = data["timeframe"];
+    const parameters = data["parameters"];
+
+    let result;
+    if (functionIdentifier && functionIdentifier !== "") {
+      // function invocation query
+
+      result = await queryFunctionInvocation(functionIdentifier);
+    } else {
+      //event query
+      result = await queryEvent(eventIdentifier, filter, timeframe, parameters);
+    }
+
+    // TimeRange: { startTime: 0, endTime: 1669039604014000 },
+
+    res.json({ result: result });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/subscribe", async (req, res, next) => {
+  try {
+    const data = req.body;
+
+    subscriptions.push(devNftSub);
+    res.json({ result: "successful" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/unsubscribe", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    await callMove();
+    res.json({ message: "from index api" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/txdata", async (req, res, next) => {
+  try {
+    const txId = req.body["txId"];
+    const txEvents = await provider.getTransactionWithEffects(txId);
+    res.json({ result: txEvents });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running`);
+});
+
+const callMove = async (
+  packageObjectId,
+  module,
+  function_name,
+  typeArguments,
+  call_arguments
+) => {
   const signer = new RawSigner(keypair, provider);
   const moveCallTxn = await signer.executeMoveCall({
-    packageObjectId: "0x2",
-    module: "devnet_nft",
-    function: "mint",
-    typeArguments: [],
-    arguments: [
-      "Example NFT",
-      "An NFT created by the wallet Command Line Tool",
-      "ipfs://bafkreibngqhl3gaa7daob4i2vccziay2jjlp435cf66vhono7nrvww53ty",
-    ],
+    packageObjectId: packageObjectId,
+    module: module,
+    function: function_name,
+    typeArguments: typeArguments,
+    arguments: call_arguments,
     gasBudget: 10000,
   });
   console.log("moveCallTxn", moveCallTxn);
 };
 
-await callMove();
+const scanForNewTransactions = async () => {
+  console.log("Start to scan for new transactions");
+  const newTxCount = await provider.getTotalTransactionNumber();
+  if (lastTransactionScannedCount < newTxCount) {
+    console.log(
+      `Found new transactions: ${newTxCount - lastTransactionScannedCount}`
+    );
+    lastTransactionScannedCount = newTxCount;
+  }
+  console.log("End scanning transactions");
+};
+
+setInterval(scanForNewTransactions, 1000 * 10);
+
+const queryEvent = async (eventIdentifier, filter, timeframe, parameters) => {
+  const eventFilter = { MoveEvent: eventIdentifier };
+  const result = await provider.getEvents(eventFilter);
+  return transformQueryResult(result["data"]);
+};
+
+const queryFunctionInvocation = async (
+  functionIdentifier,
+  filter,
+  timeframe,
+  parameters
+) => {
+  const info = functionIdentifier.split("/");
+  // Todo: Transform result
+  const devnetNftFilter = {
+    MoveModule: { package: info[0], module: info[1] },
+  };
+  const result = await provider.getEvents(devnetNftFilter);
+  return result;
+};
+
+const transformQueryResult = (data) => {
+  let result = [];
+
+  for (let i = 0; i < data.length; i++) {
+    const fields = data[i]["event"]["moveEvent"]["fields"];
+    const keys = Object.keys(fields);
+    const parameters = [];
+    for (let j = 0; j < keys.length; j++) {
+      parameters.push({
+        name: keys[j],
+        value: fields[keys[j]],
+      });
+    }
+    result.push({
+      timestamp: data[i]["timestamp"],
+      parameters: fields,
+    });
+  }
+
+  return result;
+};
